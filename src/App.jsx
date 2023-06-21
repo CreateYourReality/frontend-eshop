4a39f2f9287152a0c84a28dd4441675a5fbb8a05
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-06-21 17:10:06 +0200

    day one detailpage, addToCartBtn, goBackHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom"
-import './App.css'
-import { useState } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
 import ProductList from "./pages/ProductList";
 import Home from "./pages/Home";
-import Details from "./pages/Details";
+import Details from "./pages/details/Details.jsx";
 import LoadingScreen from "./pages/LoadingScreen";
-import { loadingContext,dataContext } from "./context/Context";
+import { loadingContext, dataContext } from "./context/Context";
 
 function App() {
-  const [loading,setLoading] = useState(true);
-  const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState([]);
 
-  return (
-    <>
-      <main>
-        <loadingContext.Provider value={{ loading, setLoading }}>
-          <dataContext.Provider value={{data, setData}}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={loading?<LoadingScreen/>:<Home/>}/>
-              <Route path="/productlist" element={loading?<LoadingScreen/>:<ProductList/>}/>
-              <Route path="/details" element={loading?<LoadingScreen/>:<Details/>}/>
-            </Routes>
-          </BrowserRouter>
-          </dataContext.Provider>
-        </loadingContext.Provider>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<main>
+				<loadingContext.Provider value={{ loading, setLoading }}>
+					<dataContext.Provider value={{ data, setData }}>
+						<BrowserRouter>
+							<Routes>
+								<Route
+									path='/home'
+									element={loading ? <LoadingScreen /> : <Home />}
+								/>
+								<Route
+									path='/productlist'
+									element={loading ? <LoadingScreen /> : <ProductList />}
+								/>
+								<Route
+									path='/'
+									element={loading ? <LoadingScreen /> : <Details />}
+								/>
+							</Routes>
+						</BrowserRouter>
+					</dataContext.Provider>
+				</loadingContext.Provider>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,75 @@
+import { useContext, useState } from "react";
+import { dataContext } from "../../context/Context";
+import { useParams } from "react-router-dom";
+import "./Details.css";
+import AddToCartBtn from "../../components/addToCartBtn/AddToCartBtn";
+import GoBackHeader from "../../components/goBackHeader/GoBackHeader";
+
+const Details = () => {
+	const { data, setData } = useContext(dataContext);
+	{
+		console.log(data);
+	}
+
+	const params = useParams();
+
+	//!Test ID
+	const testId = 25;
+
+	// Nur auf die Produkte zugreifen
+	const productArray = data.products;
+
+	//das Object finden mit der passenden ID (PARAM)
+	const selectedProduct = productArray.find(item => item.id === testId);
+
+	// * Counter
+	const [counter, setCounter] = useState(1);
+
+	const addOne = () => {
+		setCounter(prevCount => prevCount + 1);
+	};
+
+	const subOne = () => {
+		setCounter(prevCount => prevCount - 1);
+	};
+
+	return (
+		<>
+			<GoBackHeader text={selectedProduct.title} />
+			<section className='detail-sec'>
+				{selectedProduct ? (
+					<article>
+						<img src={selectedProduct.images[0]} alt={selectedProduct.title} />
+						<div className='title-div'>
+							<h2>{selectedProduct.title}</h2>
+							<div className='button-div'>
+								<button onClick={subOne}>-</button>
+								<p className='counter-state'>{counter}</p>
+								<button onClick={addOne}>+</button>
+							</div>
+						</div>
+						<img
+							className='star'
+							src='../src/pages/details/Star.png'
+							alt='Star'
+						/>
+						<p className='product-rating'>{selectedProduct.rating}</p>
+						<div className='price-div'>
+							<p>{selectedProduct.stock} in stock</p>
+							<h3>${selectedProduct.price}</h3>
+						</div>
+						<div className='description-div'>
+							<h4>Description</h4>
+							<p>{selectedProduct.description}</p>
+						</div>
+						<AddToCartBtn />
+					</article>
+				) : (
+					<p>Loading Data...</p>
+				)}
+			</section>
+		</>
+	);
+};
+
+export default Details;
