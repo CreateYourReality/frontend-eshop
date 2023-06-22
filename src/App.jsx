be8eecd579611e2777f9759a6f11ce0851a80ee0
Alperen Ertas <alpy81@MBP-von-Alperen.fritz.box>
2023-06-22 09:35:57 +0200

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom"
-import './App.css'
-import { useState } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
 import ProductList from "./pages/ProductList";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 import LoadingScreen from "./pages/LoadingScreen";
-import { loadingContext,dataContext } from "./context/Context";
+import { loadingContext, dataContext } from "./context/Context";
+import Onboarding from "./pages/Onboarding";
 
 function App() {
-  const [loading,setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
   return (
     <>
       <main>
         <loadingContext.Provider value={{ loading, setLoading }}>
-          <dataContext.Provider value={{data, setData}}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={loading?<LoadingScreen/>:<Home/>}/>
-              <Route path="/productlist" element={loading?<LoadingScreen/>:<ProductList/>}/>
-              <Route path="/details" element={loading?<LoadingScreen/>:<Details/>}/>
-            </Routes>
-          </BrowserRouter>
+          <dataContext.Provider value={{ data, setData }}>
+            <BrowserRouter>
+              <Routes>
+                <Route
+                  path="/"
+                  element={loading ? <LoadingScreen /> : <Onboarding />}
+                />
+                <Route
+                  path="/home"
+                  element={loading ? <LoadingScreen /> : <Home />}
+                />
+                <Route
+                  path="/productlist"
+                  element={loading ? <LoadingScreen /> : <ProductList />}
+                />
+                <Route
+                  path="/details"
+                  element={loading ? <LoadingScreen /> : <Details />}
+                />
+              </Routes>
+            </BrowserRouter>
           </dataContext.Provider>
         </loadingContext.Provider>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Onboarding.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Onboarding.css";
+import logo from "./logo.png";
+import { Link } from "react-router-dom";
+
+const Onboarding = () => {
+  return (
+    <section className="onboarding-section">
+      <div className="illustration">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="h-tag">
+        <h2>Biggest Sell at Your Fingerprint</h2>
+      </div>
+      <div className="p-tag">
+        <p>Find your best products from popular shop without any delay</p>
+      </div>
+      <Link to="/home">Get started</Link>
+    </section>
+  );
+};
+
+export default Onboarding;
