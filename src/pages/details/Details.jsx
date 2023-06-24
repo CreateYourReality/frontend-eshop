5f30ea8788d95873415c286db4950c606482b54d
Quaselz <paul.geppert94@freenet.de>
2023-06-24 18:13:02 +0200

    finish cart

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -7,6 +7,8 @@ import GoBackHeader from "../../components/goBackHeader/GoBackHeader";
 import Footer from "../../components/Footer/Footer";
 import star from "../../assets/img/Star.png";
 import AddToFavBtn from "../../components/addToFavBtn/addToFavBtn";
+import MinusSvg from "../../components/svgComponents/minus/MinusSvg";
+import PlusSvg from "../../components/svgComponents/plus/PlusSvg";
 
 const Details = () => {
 	const { data, setData } = useContext(dataContext);
@@ -56,49 +58,11 @@ const Details = () => {
 									<h2>{selectedProduct.title}</h2>
 									<div className='button-div'>
 										<button onClick={subOne}>
-											<svg
-												width='16'
-												height='16'
-												viewBox='0 0 16 16'
-												fill='none'
-												xmlns='http://www.w3.org/2000/svg'
-											>
-												<g id='Frame' clipPath='url(#clip0_1_1371)'>
-													<path
-														id='Vector'
-														d='M7.33334 7.33338L8.66668 7.33337L12.6667 7.33338V8.66671H8.66668L7.33334 8.66672L3.33334 8.66671V7.33338H7.33334Z'
-														fill='white'
-													/>
-												</g>
-												<defs>
-													<clipPath id='clip0_1_1371'>
-														<rect width='16' height='16' fill='white' />
-													</clipPath>
-												</defs>
-											</svg>
+											<MinusSvg />
 										</button>
 										<p className='counter-state'>{counter}</p>
 										<button onClick={addOne}>
-											<svg
-												width='16'
-												height='16'
-												viewBox='0 0 16 16'
-												fill='none'
-												xmlns='http://www.w3.org/2000/svg'
-											>
-												<g id='Frame' clipPath='url(#clip0_1_1368)'>
-													<path
-														id='Vector'
-														d='M7.33334 7.33337V3.33337H8.66668V7.33337H12.6667V8.66671H8.66668V12.6667H7.33334V8.66671H3.33334V7.33337H7.33334Z'
-														fill='white'
-													/>
-												</g>
-												<defs>
-													<clipPath id='clip0_1_1368'>
-														<rect width='16' height='16' fill='white' />
-													</clipPath>
-												</defs>
-											</svg>
+											<PlusSvg />
 										</button>
 									</div>
 								</div>
