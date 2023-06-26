c0aefafffc98bea5586aa84043baccb3616f3dbd
CreateYourReality <timo.moewes@icloud.com>
2023-06-26 21:45:11 +0200

    style admin

--- src/pages/loadingScreen/LoadingScreen.jsx
+++ src/pages/loadingScreen/LoadingScreen.jsx
@@ -4,27 +4,34 @@ import ShoppingBag from "../../assets/svg/ShoppingBag";
 import "./LoadingScreen.css";
 import EshopFont from "../../assets/svg/EshopFont";
 
+import newData from "../../assets/FakeShop/test.json";
+import userData from "../../assets/FakeShop/users.json"
+
 const LoadingScreen = () => {
 	const { setData } = useContext(dataContext);
 	const { setLoading } = useContext(loadingContext);
 	const { setUsers } = useContext(usersContext);
 
 	useEffect(() => {
-        fetch("../src/assets/FakeShop/users.json")
+   /*     fetch("../src/assets/FakeShop/users.json")
         .then(res => res.json())
-        .then(userData => setUsers(userData))
+        .then(userData => setUsers(userData)) */
+		setUsers(userData)
     }, [])
 
 	useEffect(() => {
 		//const url = "http://feuerwerkankreativitaet.biz/data.json";
 		//const myJSON = JSON.stringify(url);
 		//fetch("http://feuerwerkankreativitaet.biz/test.json")
-		fetch("../src/assets/FakeShop/test.json")
+	/*	fetch("../src/assets/FakeShop/test.json")
 			.then(res => res.json())
 			.then(data => {
 				setData(data);
-			});
+			}); */
+
+			setData(newData)
 	}, []);
+
 	const DeactivateLoading = () => {
 		setTimeout(stopLoading, 2500);
 	};
